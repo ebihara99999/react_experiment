a4a49f200116588187f43b6549814791377e289c
Yusuke Ebihara <biibiebi1234@gmail.com>
2017-05-13 15:10:22 +0900

    Create webpack config files

---
+++ development.js
@@ -0,0 +1,28 @@
+import path from 'path';
+
+const src = path.resolve(__dirname, 'src');
+const dist = path.resolve(__dirname, 'dist');
+
+export default {
+  entry: `${src}/index.jsx`,
+
+  output: {
+    path: dist,
+    filename: 'bundle.js',
+  },
+
+  module: {
+    loaders: [
+      {
+        test: /\.jsx$/,
+        exclude: /node_modules/,
+        loader: 'babel-loader',
+      },
+    ],
+  },
+  resolve: {
+    extensions: ['.js'],
+  },
+
+  plugins: [],
+};
