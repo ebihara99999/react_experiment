4d8c265d9cc2a9bb4f5f8e65d2560141aeb66671
Yusuke Ebihara <biibiebi1234@gmail.com>
2017-05-13 14:42:44 +0900

    Add eslint

---
+++ .eslintrc.js
@@ -0,0 +1,14 @@
+module.exports = {
+    "extends": "google",
+    "env": {
+        "node": true,
+        "es6": true
+    },
+    "parserOptions": {
+        "sourceType": "module"
+    },
+    "rules": {
+        // 改行コードはUnix
+        "linebreak-style": ["error", "unix"]
+    }
+}
