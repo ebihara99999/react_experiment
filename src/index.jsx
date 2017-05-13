08d53513312f0334149e2f1533ec320e8a3154b4
Yusuke Ebihara <biibiebi1234@gmail.com>
2017-05-13 15:18:29 +0900

    Update index.jsx

--- src/index.jsx
+++ src/index.jsx
@@ -1,9 +1,23 @@
-import React from 'react';
-import {render} from 'react-dom';
+import React from 'react'
+import { render } from 'react-dom'
 
 class App extends React.Component {
+  constructor(props) { 
+    super(props)
+    this.state = { message: 'something' }
+  }
+
+  onChange(e) {
+     this.setState( {message: e.target.value} )
+  }
+
   render() {
-    return <p> Hellor React! </p>
+    return (
+      <div>
+        <input type="text" onChange = { this.onChange.bind(this) } />
+        <p>{ this.state.message }</p>
+      </div>
+    )
   }
 }
 
